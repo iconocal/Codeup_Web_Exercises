75d3d8fcd006904e7d3ed64b0b39f40445fd1311
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-15 10:53:53 -0600

    iterating arrays exercise initial commit

---
+++ public/js/iterating_arrays.js
@@ -0,0 +1,18 @@
+// todo:
+// 1. Create an array of 4 names using literal array notation, in a variable called 'names'.
+// 2. Create a log statement that will log the number of elements in the names array.
+// 3. Create log statements that will print each of the names array elements individually.
+
+
+var names = ['Calvin', 'Kady', 'Patrick', 'Mardi'];
+
+
+console.log(names.length);
+
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+
