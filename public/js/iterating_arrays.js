d58d377b27a6e8703fb919f3797a2efeb784591d
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-15 11:31:27 -0600

    iterating arrays with for, foreach, and alt syntax for each

--- public/js/iterating_arrays.js
+++ public/js/iterating_arrays.js
@@ -4,15 +4,45 @@
 // 3. Create log statements that will print each of the names array elements individually.
 
 
+// var names = ['Calvin', 'Kady', 'Patrick', 'Mardi'];
+
+
+// console.log(names.length);
+
+
+// // console.log(names[0]);
+// // console.log(names[1]);
+// // console.log(names[2]);
+// // console.log(names[3]);
+
+
+// // ForEach Iteration through arrays
+
+// // loop through the array and log the values
+// // element is the value, index is the key
+// shapes.forEach(function (element, index, array) {
+//     console.log('The shape at index ' + index + ' is: ' + element);
+// });
+
+
+
 var names = ['Calvin', 'Kady', 'Patrick', 'Mardi'];
 
+// for (var i = 0; i < names.length; i++) {
+// 	console.log('The name at index ' + i + ' is: ' + names[i]);
+// }
 
-console.log(names.length);
+// forEach iteration through arrays
+names.forEach(function (name, i, array) {
+	console.log('The name at index ' + i + ' is ' + name);
+});
 
 
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
+// // Alternate Syntax for forEach loop iteration
+// function loopArray(value, key, array) {
+// 	console.log('The name at index ' + key + ' is ' + value);
+// }
+
+// names.forEach(loopArray);
 
 
