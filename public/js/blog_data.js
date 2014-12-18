766a77378a5e9823cf0527162ebeb60bd24e9b82
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-18 15:47:02 -0600

    JSON Blog Exercise initial commit

---
+++ public/js/blog_data.js
@@ -0,0 +1,21 @@
+var data = [
+	{
+		"title": "Blog Post",
+		"date": "2014-12-14",
+		"author": "Calvin Glover",
+		"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet nam placeat, officiis porro necessitatibus magni iste, eum voluptas laborum minus quos quas. Omnis necessitatibus, hic earum sint! Inventore dolorum doloremque, optio pariatur, omnis necessitatibus odit atque. Voluptatum, distinctio commodi, expedita rerum molestiae atque aliquam quis, quo architecto debitis consequuntur numquam."
+	},
+	{
+		"title": "Blog Post Two",
+		"date": "2014-12-15",
+		"author": "Calvin Glover",
+		"content": "Hipster ipsum dolor sit amet, consectetur adipisicing elit. Eveniet nam placeat, officiis porro necessitatibus magni iste, eum voluptas laborum minus quos quas. Omnis necessitatibus, hic earum sint! Inventore dolorum doloremque, optio pariatur, omnis necessitatibus odit atque. Voluptatum, distinctio commodi, expedita rerum molestiae atque aliquam quis, quo architecto debitis consequuntur numquam."	
+	},
+	{
+		"title": "Blog Post Three",
+		"date": "2014-12-17",
+		"author": "Calvin Glover",
+		"content": "Hipster doofus ipsum dolor sit amet, consectetur adipisicing elit. Eveniet nam placeat, officiis porro necessitatibus magni iste, eum voluptas laborum minus quos quas. Omnis necessitatibus, hic earum sint! Inventore dolorum doloremque, optio pariatur, omnis necessitatibus odit atque. Voluptatum, distinctio commodi, expedita rerum molestiae atque aliquam quis, quo architecto debitis consequuntur numquam."	
+	}
+
+];
