5f6d45aa6f32420423b59bcbe26de9c9dd7c7d06
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-19 15:32:06 -0600

    JSON Blog and Disarm BOM

--- public/js/blog_data.js
+++ public/js/blog_data.js
@@ -3,7 +3,7 @@ var data = [
 		"title": "Blog Post",
 		"date": "2014-12-14",
 		"author": "Calvin Glover",
-		"content": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet nam placeat, officiis porro necessitatibus magni iste, eum voluptas laborum minus quos quas. Omnis necessitatibus, hic earum sint! Inventore dolorum doloremque, optio pariatur, omnis necessitatibus odit atque. Voluptatum, distinctio commodi, expedita rerum molestiae atque aliquam quis, quo architecto debitis consequuntur numquam."
+		"content": "In the agile workflow space, industry is globally deep-diving its best-of-breed capabilities. Key players will take ownership of their paradigm shifts by ethically growing competitive enterprises. In the bandwidth space, industry is virtually synergising its mobile visibilities. Efficiencies will come from intelligently calibrating our alignments."
 	},
 	{
 		"title": "Blog Post Two",
